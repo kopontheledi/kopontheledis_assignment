d636f0a3c17f1124ac30a7f933c5a152bc629229
Kopo Ntheledi <kopontheledi@gmail.com>
2024-11-14 06:15:57 +0200

    update

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,17 @@
-// index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import App from './public/App';
-import ContactForm from './components/contactForm'; 
-import ContactList from './components/clientList';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <Router>
-    <Switch>
-      <Route exact path="/" component={App} />
-      <Route path="/contact-form" component={ContactForm} />
-      <Route path="/contact-summary" component={ContactList} />
-    </Switch>
-  </Router>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
